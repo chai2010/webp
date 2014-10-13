ca2f160cefaa6e2e4b9624726d38d70a5af1936c
chai2010 <chaishushan@gmail.com>
2014-10-13 13:11:18 +0800

    remove unused files

--- internal/libwebp/src/webp_demux.c
+++
@@ -1,6 +0,0 @@
-// Copyright 2014 <chaishushan{AT}gmail.c"om>. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-#include "./demux/demux.c"
-

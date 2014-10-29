c2cdeab06ff2d9bccc49f3d924d3e604c289c077
chai2010 <chaishushan@gmail.com>
2014-10-29 10:04:00 +0800

    redesign Image interface

--- image.go
+++ image.go
@@ -14,19 +14,17 @@ type Image interface {
 	// Get original type, such as *image.Gray, *image.RGBA, etc.
 	BaseType() image.Image
 
-	// Pix holds the image's pixels, as pixel values in big-endian format. The pixel at
-	// (x, y) starts at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*Channels*sizeof(DataType)].
+	// Pix holds the image's pixels, as pixel values in machine order format. The pixel at
+	// (x, y) starts at []Type(Pix[(y-Rect.Min.Y)*Stride:])[(x-Rect.Min.X)*Channels:].
 	Pix() []byte
 	// Stride is the Pix stride (in bytes) between vertically adjacent pixels.
 	Stride() int
 	// Rect is the image's bounds.
 	Rect() image.Rectangle
 
-	// if Depth() != Invalid { 1:Gray, 2:GrayA, 3:RGB, 4:RGBA, N:[N]Type }
-	// if Depth() == Invalid { N:[N]byte }
+	// 1:Gray, 2:GrayA, 3:RGB, 4:RGBA
 	Channels() int
-	// Invalid/Uint8/Uint16/Uint32/Uint64/Int8/Int16/Int32/Int64/Float32/Float64
-	// Invalid is equal Byte type.
+	// Uint8/Uint16/Int32/Int64/Float32/Float64
 	Depth() reflect.Kind
 
 	draw.Image

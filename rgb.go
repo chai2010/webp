8b63aceb8c3e4fbdec5103a62a1c37aeee5a2b0a
chai2010 <chaishushan@gmail.com>
2015-07-22 16:32:31 +0800

    sync RGBImage type, see https://github.com/chai2010/image

--- rgb.go
+++ rgb.go
@@ -10,33 +10,14 @@ import (
 )
 
 var (
-	_ color.Color = (*RGBColor)(nil)
+	_ RGBImager   = (*RGBImage)(nil)
 	_ image.Image = (*RGBImage)(nil)
 )
 
-var RGBModel color.Model = color.ModelFunc(rgbModel)
-
-type RGBColor struct {
-	R, G, B uint8
-}
-
-func (c RGBColor) RGBA() (r, g, b, a uint32) {
-	r = uint32(c.R)
-	r |= r << 8
-	g = uint32(c.G)
-	g |= g << 8
-	b = uint32(c.B)
-	b |= b << 8
-	a = 0xffff
-	return
-}
-
-func rgbModel(c color.Color) color.Color {
-	if _, ok := c.(RGBColor); ok {
-		return c
-	}
-	r, g, b, _ := c.RGBA()
-	return RGBColor{R: uint8(r >> 8), G: uint8(g >> 8), B: uint8(b >> 8)}
+type RGBImager interface {
+	image.Image
+	RGBAt(x, y int) [3]uint8
+	SetRGB(x, y int, c [3]uint8)
 }
 
 type RGBImage struct {
@@ -50,18 +31,27 @@ func (p *RGBImage) ColorModel() color.Model { return color.RGBAModel }
 func (p *RGBImage) Bounds() image.Rectangle { return p.Rect }
 
 func (p *RGBImage) At(x, y int) color.Color {
-	return p.RGBAt(x, y)
-}
-
-func (p *RGBImage) RGBAt(x, y int) RGBColor {
 	if !(image.Point{x, y}.In(p.Rect)) {
-		return RGBColor{}
+		return color.RGBA{}
 	}
 	i := p.PixOffset(x, y)
-	return RGBColor{
+	return color.RGBA{
 		R: p.Pix[i+0],
 		G: p.Pix[i+1],
 		B: p.Pix[i+2],
+		A: 0xff,
+	}
+}
+
+func (p *RGBImage) RGBAt(x, y int) [3]uint8 {
+	if !(image.Point{x, y}.In(p.Rect)) {
+		return [3]uint8{}
+	}
+	i := p.PixOffset(x, y)
+	return [3]uint8{
+		p.Pix[i+0],
+		p.Pix[i+1],
+		p.Pix[i+2],
 	}
 }
 
@@ -76,21 +66,21 @@ func (p *RGBImage) Set(x, y int, c color.Color) {
 		return
 	}
 	i := p.PixOffset(x, y)
-	c1 := RGBModel.Convert(c).(RGBColor)
+	c1 := color.RGBAModel.Convert(c).(color.RGBA)
 	p.Pix[i+0] = c1.R
 	p.Pix[i+1] = c1.G
 	p.Pix[i+2] = c1.B
 	return
 }
 
-func (p *RGBImage) SetRGB(x, y int, c RGBColor) {
+func (p *RGBImage) SetRGB(x, y int, c [3]uint8) {
 	if !(image.Point{x, y}.In(p.Rect)) {
 		return
 	}
 	i := p.PixOffset(x, y)
-	p.Pix[i+0] = c.R
-	p.Pix[i+1] = c.G
-	p.Pix[i+2] = c.B
+	p.Pix[i+0] = c[0]
+	p.Pix[i+1] = c[1]
+	p.Pix[i+2] = c[2]
 	return
 }
 
@@ -139,10 +129,10 @@ func NewRGBImageFrom(m image.Image) *RGBImage {
 	for y := b.Min.Y; y < b.Max.Y; y++ {
 		for x := b.Min.X; x < b.Max.X; x++ {
 			pr, pg, pb, _ := m.At(x, y).RGBA()
-			rgb.SetRGB(x, y, RGBColor{
-				R: uint8(pr >> 8),
-				G: uint8(pg >> 8),
-				B: uint8(pb >> 8),
+			rgb.SetRGB(x, y, [3]uint8{
+				uint8(pr >> 8),
+				uint8(pg >> 8),
+				uint8(pb >> 8),
 			})
 		}
 	}

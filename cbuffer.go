2fc75dd2c00ec97a7da04a2cec6b044f596d52e3
chai2010 <chaishushan@gmail.com>
2015-08-10 06:19:11 +0800

    CBuffer support auto finalizer

--- cbuffer.go
+++ cbuffer.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"io"
 	"reflect"
+	"runtime"
 	"unsafe"
 )
 
@@ -27,13 +28,42 @@ type CBuffer interface {
 }
 
 type cBuffer struct {
+	*innerCBuffer
+}
+
+func newCBufferFrom(cptr unsafe.Pointer, size int, dontResize ...bool) CBuffer {
+	p := &cBuffer{
+		innerCBuffer: newInnerCBufferFrom(cptr, size, dontResize...),
+	}
+	runtime.SetFinalizer(p.innerCBuffer, (*innerCBuffer).Close)
+	return p
+}
+
+func NewCBuffer(size int, dontResize ...bool) CBuffer {
+	p := &cBuffer{
+		innerCBuffer: newInnerCBuffer(size, dontResize...),
+	}
+	runtime.SetFinalizer(p.innerCBuffer, (*innerCBuffer).Close)
+	return p
+}
+
+func (p *cBuffer) Close() error {
+	var err error
+	if p.innerCBuffer != nil {
+		err = p.innerCBuffer.Close()
+	}
+	*p = cBuffer{}
+	return err
+}
+
+type innerCBuffer struct {
 	dontResize bool
 	cptr       unsafe.Pointer
 	data       []byte
 }
 
-func newCBufferFrom(cptr unsafe.Pointer, size int, dontResize ...bool) CBuffer {
-	p := new(cBuffer)
+func newInnerCBufferFrom(cptr unsafe.Pointer, size int, dontResize ...bool) *innerCBuffer {
+	p := new(innerCBuffer)
 	if cptr != nil && size > 0 {
 		p.cptr = cptr
 		p.data = (*[1 << 30]byte)(p.cptr)[0:size:size]
@@ -44,8 +74,8 @@ func newCBufferFrom(cptr unsafe.Pointer, size int, dontResize ...bool) CBuffer {
 	return p
 }
 
-func NewCBuffer(size int, dontResize ...bool) CBuffer {
-	p := new(cBuffer)
+func newInnerCBuffer(size int, dontResize ...bool) *innerCBuffer {
+	p := new(innerCBuffer)
 	if size > 0 {
 		p.cptr = C.webpMalloc(C.size_t(size))
 		p.data = (*[1 << 30]byte)(p.cptr)[0:size:size]
@@ -56,29 +86,36 @@ func NewCBuffer(size int, dontResize ...bool) CBuffer {
 	return p
 }
 
-func (p *cBuffer) CBufMagic() string {
+func (p *innerCBuffer) CBufMagic() string {
 	return "CBufMagic"
 }
 
-func (p *cBuffer) Close() error {
+func (p *innerCBuffer) Close() error {
 	if p.cptr != nil {
 		C.webpFree(p.cptr)
 	}
-	p.cptr = nil
-	p.data = nil
+	*p = innerCBuffer{}
+
+	// no need for a finalizer anymore
+	runtime.SetFinalizer(p, nil)
 	return nil
 }
 
-func (p *cBuffer) CanResize() bool {
+func (p *innerCBuffer) CanResize() bool {
 	return !p.dontResize
 }
 
-func (p *cBuffer) Resize(size int) error {
+func (p *innerCBuffer) Resize(size int) error {
 	if size < 0 {
 		return errors.New("webp: cBuffer.Resize, bad size!")
 	}
 	if p.dontResize {
 		return errors.New("webp: cBuffer.Resize, donot resize!")
+	}
+	if n := len(p.data); n > 0 && n != size {
+		C.webpFree(p.cptr)
+		p.cptr = nil
+		p.data = nil
 	}
 	p.Close()
 	if size > 0 {
@@ -88,11 +125,11 @@ func (p *cBuffer) Resize(size int) error {
 	return nil
 }
 
-func (p *cBuffer) CData() []byte {
+func (p *innerCBuffer) CData() []byte {
 	return p.data
 }
 
-func (p *cBuffer) Own(d []byte) bool {
+func (p *innerCBuffer) Own(d []byte) bool {
 	if cap(d) == 0 || p.cptr == nil {
 		return false
 	}

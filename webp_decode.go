f350dc0eaf52bbc342c99d9419f64cc801f576cd
chai2010 <chaishushan@gmail.com>
2016-02-11 16:51:13 +0800

    export C.WebPGetInfo

--- webp_decode.go
+++ webp_decode.go
@@ -6,6 +6,7 @@ package webp
 
 //#include <webp/decode.h>
 import "C"
+import "unsafe"
 
 const (
 	WEBP_DECODER_ABI_VERSION = 0x0203 // MAJOR(8b) + MINOR(8b)
@@ -19,4 +20,19 @@ const (
 // each of major/minor/revision. E.g: v2.5.7 is 0x020507.
 func WebPGetDecoderVersion() uint {
 	return uint(C.WebPGetDecoderVersion())
+}
+
+// Retrieve basic header information: width, height.
+// This function will also validate the header and return 0 in
+// case of formatting error.
+// Pointers 'width' and 'height' can be passed NULL if deemed irrelevant.
+func WebPGetInfo(data []byte) (width, height int, ok bool) {
+	if len(data) == 0 {
+		return 0, 0, false
+	}
+	var cw, ch C.int
+	if C.WebPGetInfo((*C.uint8_t)(unsafe.Pointer(&data[0])), C.size_t(len(data)), &cw, &ch) == 0 {
+		return 0, 0, false
+	}
+	return int(cw), int(ch), true
 }
--- webp_decode_test.go
+++ webp_decode_test.go
@@ -5,9 +5,20 @@
 package webp
 
 import (
+	"io/ioutil"
 	"testing"
 )
 
 func TestWEBP_DECODER_ABI_VERSION(t *testing.T) {
 	tAssertEQ(t, _C_WEBP_DECODER_ABI_VERSION, WEBP_DECODER_ABI_VERSION)
+}
+
+func TestWebPGetInfo(t *testing.T) {
+	data, err := ioutil.ReadFile("./testdata/1_webp_ll.webp")
+	tAssertNil(t, err)
+
+	w, h, ok := WebPGetInfo(data)
+	tAssertEQ(t, 400, w)
+	tAssertEQ(t, 301, h)
+	tAssert(t, ok)
 }

105bcc083e27734281e19fdc17b0fdf74f864846
chai2010 <chaishushan@gmail.com>
2016-02-13 01:49:57 +0800

    add fuzz

---
+++ fuzz/webp_fuzz.go
@@ -0,0 +1,26 @@
+// Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// See https://github.com/dvyukov/go-fuzz
+package fuzz
+
+import (
+	"bytes"
+
+	"github.com/chai2010/webp"
+)
+
+func Fuzz(data []byte) int {
+	cfg, err := webp.DecodeConfig(bytes.NewReader(data))
+	if err != nil {
+		return 0
+	}
+	if cfg.Width*cfg.Height > 1e6 {
+		return 0
+	}
+	if _, err := webp.Decode(bytes.NewReader(data)); err != nil {
+		return 0
+	}
+	return 1
+}

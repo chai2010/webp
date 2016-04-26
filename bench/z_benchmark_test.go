ad4e0e9e55d2e06d01dfde9120f87dee2a666c36
James Schofield <james@shoeboxapp.com>
2016-04-26 10:41:35 -0400

    Adding large lossy photo and additional benchmarks.

--- bench/z_benchmark_test.go
+++ bench/z_benchmark_test.go
@@ -47,6 +47,18 @@ func BenchmarkDecode_1_webp_a_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_1_webp_a_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "1_webp_a.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_1_webp_ll_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "1_webp_ll.webp")
 	b.ResetTimer()
@@ -64,6 +76,18 @@ func BenchmarkDecode_1_webp_ll_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_1_webp_ll_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "1_webp_ll.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -95,6 +119,18 @@ func BenchmarkDecode_2_webp_a_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_2_webp_a_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "2_webp_a.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_2_webp_ll_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "2_webp_ll.webp")
 	b.ResetTimer()
@@ -112,6 +148,18 @@ func BenchmarkDecode_2_webp_ll_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_2_webp_ll_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "2_webp_ll.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -143,6 +191,18 @@ func BenchmarkDecode_3_webp_a_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_3_webp_a_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "3_webp_a.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_3_webp_ll_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "3_webp_ll.webp")
 	b.ResetTimer()
@@ -160,6 +220,18 @@ func BenchmarkDecode_3_webp_ll_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_3_webp_ll_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "3_webp_ll.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -191,6 +263,18 @@ func BenchmarkDecode_4_webp_a_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_4_webp_a_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "4_webp_a.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_4_webp_ll_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "4_webp_ll.webp")
 	b.ResetTimer()
@@ -208,6 +292,18 @@ func BenchmarkDecode_4_webp_ll_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_4_webp_ll_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "4_webp_ll.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -239,6 +335,18 @@ func BenchmarkDecode_5_webp_a_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_5_webp_a_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "5_webp_a.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_5_webp_ll_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "5_webp_ll.webp")
 	b.ResetTimer()
@@ -256,6 +364,18 @@ func BenchmarkDecode_5_webp_ll_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_5_webp_ll_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "5_webp_ll.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -287,6 +407,18 @@ func BenchmarkDecode_blue_purple_pink_large_lossless_x_image_webp(b *testing.B) 
 	}
 }
 
+func BenchmarkDecode_blue_purple_pink_large_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "blue-purple-pink-large.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_blue_purple_pink_large_no_filter_lossy_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "blue-purple-pink-large.no-filter.lossy.webp")
 	b.ResetTimer()
@@ -304,6 +436,18 @@ func BenchmarkDecode_blue_purple_pink_large_no_filter_lossy_x_image_webp(b *test
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_blue_purple_pink_large_no_filter_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "blue-purple-pink-large.no-filter.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -335,6 +479,18 @@ func BenchmarkDecode_blue_purple_pink_large_normal_filter_lossy_x_image_webp(b *
 	}
 }
 
+func BenchmarkDecode_blue_purple_pink_large_normal_filter_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "blue-purple-pink-large.normal-filter.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_blue_purple_pink_large_simple_filter_lossy_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "blue-purple-pink-large.simple-filter.lossy.webp")
 	b.ResetTimer()
@@ -352,6 +508,18 @@ func BenchmarkDecode_blue_purple_pink_large_simple_filter_lossy_x_image_webp(b *
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_blue_purple_pink_large_simple_filter_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "blue-purple-pink-large.simple-filter.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -383,6 +551,18 @@ func BenchmarkDecode_blue_purple_pink_lossless_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_blue_purple_pink_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "blue-purple-pink.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_blue_purple_pink_lossy_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "blue-purple-pink.lossy.webp")
 	b.ResetTimer()
@@ -400,6 +580,18 @@ func BenchmarkDecode_blue_purple_pink_lossy_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_blue_purple_pink_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "blue-purple-pink.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -431,6 +623,18 @@ func BenchmarkDecode_gopher_doc_1bpp_lossless_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_gopher_doc_1bpp_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "gopher-doc.1bpp.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_gopher_doc_2bpp_lossless_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "gopher-doc.2bpp.lossless.webp")
 	b.ResetTimer()
@@ -448,6 +652,18 @@ func BenchmarkDecode_gopher_doc_2bpp_lossless_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_gopher_doc_2bpp_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "gopher-doc.2bpp.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -479,6 +695,18 @@ func BenchmarkDecode_gopher_doc_4bpp_lossless_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_gopher_doc_4bpp_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "gopher-doc.4bpp.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_gopher_doc_8bpp_lossless_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "gopher-doc.8bpp.lossless.webp")
 	b.ResetTimer()
@@ -496,6 +724,54 @@ func BenchmarkDecode_gopher_doc_8bpp_lossless_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_gopher_doc_8bpp_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "gopher-doc.8bpp.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_photo_lossy_chai2010_webp(b *testing.B) {
+	data := tbLoadData(b, "photo.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_photo_lossy_x_image_webp(b *testing.B) {
+	data := tbLoadData(b, "photo.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_photo_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "photo.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -527,6 +803,18 @@ func BenchmarkDecode_tux_lossless_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_tux_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "tux.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_video_001_lossy_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "video-001.lossy.webp")
 	b.ResetTimer()
@@ -544,6 +832,18 @@ func BenchmarkDecode_video_001_lossy_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_video_001_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "video-001.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -575,6 +875,18 @@ func BenchmarkDecode_video_001_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_video_001_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "video-001.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_yellow_rose_lossless_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "yellow_rose.lossless.webp")
 	b.ResetTimer()
@@ -592,6 +904,18 @@ func BenchmarkDecode_yellow_rose_lossless_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_yellow_rose_lossless_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "yellow_rose.lossless.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -623,6 +947,18 @@ func BenchmarkDecode_yellow_rose_lossy_with_alpha_x_image_webp(b *testing.B) {
 	}
 }
 
+func BenchmarkDecode_yellow_rose_lossy_with_alpha_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "yellow_rose.lossy-with-alpha.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
 func BenchmarkDecode_yellow_rose_lossy_chai2010_webp(b *testing.B) {
 	data := tbLoadData(b, "yellow_rose.lossy.webp")
 	b.ResetTimer()
@@ -640,6 +976,18 @@ func BenchmarkDecode_yellow_rose_lossy_x_image_webp(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		m, err := x_image_webp.Decode(bytes.NewReader(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = m
+	}
+}
+
+func BenchmarkDecode_yellow_rose_lossy_chai2010_webp_tosize(b *testing.B) {
+	data := tbLoadData(b, "yellow_rose.lossy.webp")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m, err := chai2010_webp.DecodeRGBAToSize(data, 256, 256)
 		if err != nil {
 			b.Fatal(err)
 		}
